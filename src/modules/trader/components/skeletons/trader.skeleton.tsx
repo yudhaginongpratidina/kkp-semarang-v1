9860299b9cb635d8082f1c516d1967152a505d20
yudhaginongpratidina <dev.yudhaginongpratidina@gmail.com>
2026-04-07 14:13:51 +0700

    add skeleton for trader

--- src/modules/trader/components/skeletons/trader.skeleton.tsx
+++ src/modules/trader/components/skeletons/trader.skeleton.tsx
@@ -1,3 +1,16 @@
+import OfficerLayout from '../../layouts/trader.layout';
+import { Skeleton } from '../../../../shared/components';
+
 export default function TraderSkeleton() {
-    return <></>;
+    return (
+        <OfficerLayout>
+            <Skeleton className="min-h-12 w-1/4" />
+            <Skeleton className="min-h-12 w-full" />
+            <Skeleton className="min-h-32 w-full" />
+            <div className="w-full flex justify-between items-center">
+                <Skeleton className="min-h-12 w-1/4" />
+                <Skeleton className="min-h-12 w-1/4" />
+            </div>
+        </OfficerLayout>
+    );
 }
